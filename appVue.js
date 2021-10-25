740001ec1b632801389d9f2e5dcd4a65ec52d646
v-Make <marlen_box@mail.ru>
2021-10-25 11:29:23 +0600

    my resume second commit

--- appVue.js
+++ appVue.js
@@ -1,22 +1,34 @@
 const app = Vue.createApp({
     data() {
         return {
-            item: { },
-viewBlock : false
+            item:{},
+viewBlock : 0,
         }
     },
     methods: {
-        Post: function() {
-            axios.post('content.php')
-                .then((response) => {
-                    this.viewBlock=true;
-                    console.log(response.data);
-                    this.item = response.data;
-                })
-                .catch((error) => {
-                    console.log(error)
-                });
+        Post1: function() {
+           this.viewBlock=1;
         },
+        Post2:function() {
+            this.viewBlock=2;
+         },
+         Post3:function() {
+            this.viewBlock=3;
+         },
+         Post4:function() {
+            this.viewBlock=4;
+         },
     },
+    mounted(){
+        axios
+        .get('content.php')
+            .then((response) => {
+                console.log(response.data);
+                this.item = response.data;
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+    }
 })
 app.mount('#app')
